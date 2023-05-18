366b697eb7f7b6b6879371f1c64b0a17337d9140
Victor Dumaslan <dumaslan2001@gmail.com>
2023-05-18 14:45:58 -0700

    assignment 6

---
+++ a6/starter/encrypt-it.js
@@ -0,0 +1,57 @@
+/*
+ * Starter file 
+ */
+(function () {
+  "use strict";
+
+  /**
+   * The starting point in our program, setting up a listener
+   * for the "load" event on the window, signalling the HTML DOM has been constructed
+   * on the page. When this event occurs, the attached function (init) will be called.
+   */
+  window.addEventListener("load", init);
+
+  /**
+   * TODO: Write a function comment using JSDoc.
+   */
+  function init() {
+    // Note: In this function, we usually want to set up our event handlers
+    // for UI elements on the page.
+    console.log("Window loaded!");
+    document.getElementById("encrypt-it").addEventListener("click", handleClick);
+  }
+
+  // Add any other functions in this area (you should not implement your
+  // entire program in the init function, for similar reasons that
+  // you shouldn't write an entire Java program in the main method).
+
+  function handleClick() {
+    console.log("Button clicked!");
+    var str = document.getElementById("input-text").value;
+    str = shiftCipher(str);
+    document.getElementById("result").innerHTML = str;
+  }
+
+  /**
+ * Returns an encrypted version of the given text, where
+ * each letter is shifted alphabetically ahead by 1 letter,
+ * and 'z' is shifted to 'a' (creating an alphabetical cycle).
+ */
+  function shiftCipher(text) {
+    text = text.toLowerCase();
+    let result = "";
+    for (let i = 0; i < text.length; i++) {
+      if (text[i] < 'a' || text[i] > 'z') {
+        result += text[i];
+      } else if (text[i] == 'z') {
+        result += 'a';
+      } else { // letter is between 'a' and 'y'
+        let letter = text.charCodeAt(i);
+        let resultLetter = String.fromCharCode(letter + 1);
+        result += resultLetter;
+      }
+    }
+    return result;
+  }
+
+})();
